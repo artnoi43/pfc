113b951434a639ecec6de91a36503f169e97499c
artnoi <artdrawin@gmail.com>
2021-04-12 18:59:06 +0700

    Added infile check, and remove some test files

--- pfc.py
+++ pfc.py
@@ -11,23 +11,23 @@ defOutfile = 'files/secret'
 # initialize cli parser
 parser = argparse.ArgumentParser()
 
-# positional/optional args
+# optional args
 parser.add_argument('-i', '--inf', type=str, default=defInfile)
-parser.add_argument('-o', '--outf', type=str, default=defOutfile)
-parser.add_argument('-d', '--dec', type=bool, default=False)
+parser.add_argument('-o', '--outf',type=str, default=defOutfile)
+parser.add_argument('-d', '--dec', type=bool,default=False)
 
-# -h help is 
 args = parser.parse_args()
 
 def getPass() -> str:
-	passphrase = input('Enter passphrase: ')
+	passphrase = (input('Enter passphrase: '))
+	return str(passphrase)
 
 # deriveKey(), encrypt(), and decrypt() are from https://gist.github.com/renesugar/d8ff6d7609e870d2de5d0d4eb5f9d135#file-aes-py
 
 def deriveKey(passphrase: str, salt: bytes=None) -> [str, bytes]:
     if salt is None:
-        salt = os.urandom(8)
-    return hashlib.pbkdf2_hmac("sha256", passphrase.encode("utf8"), salt, 1000), salt
+        salt = os.urandom(32)
+    return hashlib.pbkdf2_hmac("sha256", passphrase.encode("utf8"), salt, 1048576), salt
 
 def encrypt(passphrase: str, plaintext: str) -> str:
     key, salt = deriveKey(passphrase)
@@ -37,7 +37,6 @@ def encrypt(passphrase: str, plaintext: str) -> str:
     ciphertext = aes.encrypt(iv, plaintext, None)
     return "%s-%s-%s" % (hexlify(salt).decode("utf8"), hexlify(iv).decode("utf8"), hexlify(ciphertext).decode("utf8"))
 
-
 def decrypt(passphrase: str, ciphertext: str) -> str:
     salt, iv, ciphertext = map(unhexlify, ciphertext.split("-"))
     key, _ = deriveKey(passphrase, salt)
@@ -45,15 +44,19 @@ def decrypt(passphrase: str, ciphertext: str) -> str:
     plaintext = aes.decrypt(iv, ciphertext, None)
     return plaintext.decode("utf8")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-	infile = open(args.inf, 'r+')
-	indata = infile.read()
+	if os.path.isfile(args.inf):
+		infile = open(args.inf, 'r+')
+		indata = infile.read()
+	else:
+		print(f'File not found: {args.inf}')
+		quit(1)
 
 	if args.dec:
-		output = decrypt(str(getPass()), indata)
+		output = decrypt(getPass(), indata)
 	else:
-		output = encrypt(str(getPass()), indata)
+		output = encrypt(getPass(), indata)
 
 	if os.path.isfile(args.outf):
 		outfile = open(args.outf, 'w')
