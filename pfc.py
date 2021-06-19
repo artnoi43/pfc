22fdd28ecfb882469a107a152a0348b023c70d6f
artnoi <artdrawin@gmail.com>
2021-06-19 18:43:39 +0700

    Updated pfc.py

--- pfc.py
+++ pfc.py
@@ -2,6 +2,7 @@
 
 import argparse
 import hashlib
+import sys
 import os
 
 from getpass import getpass
@@ -10,25 +11,28 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 # Initialize cli parser
 parser = argparse.ArgumentParser()
-# Optional args
-## Input/output files
+# Input file
 parser.add_argument('-i', '--inf', type=str, default='files/plain')
+# Output file
 parser.add_argument('-o', '--outf',type=str, default='/tmp/pfc.out')
-## Decrypt flag
+# Decrypt flag
 parser.add_argument('-d', '--dec', type=bool, default=False)
-## Console input
+# Console input/output
 parser.add_argument('-t', '--text',type=bool, default=False)
+# Parse arguments
 args = parser.parse_args()
 
 lenIV = 12
 lenSalt = 32
 
+# deriveKey() derives random key from passphrase using PBKDF2
 def deriveKey(passphrase: str, salt: bytes=None) -> [str, bytes]:
 	if salt is None:
 		salt = os.urandom(lenSalt)
 	
 	return hashlib.pbkdf2_hmac("sha256", passphrase.encode("utf8"), salt, 1048576), salt
 
+# encrypt() encrypts string using AES256-GCM
 def encrypt(passphrase: str, plaintext: str) -> str:
 	# Encode string to bytes
 	plaintext = plaintext.encode("utf8")
@@ -38,9 +42,10 @@ def encrypt(passphrase: str, plaintext: str) -> str:
 	iv = os.urandom(lenIV)
 	ciphertext = aes.encrypt(iv, plaintext, None)
 
-	# Return encrypted output in hexadecimal
+	# Return encrypted output in hexadecimal string
 	return "%s%s%s" % (hexlify(ciphertext).decode("utf8"), hexlify(iv).decode("utf8"), hexlify(salt).decode("utf8"))
 
+# decrypt() decrypts AES256-GCM encrypted and hex-encoded string to plaintext string
 def decrypt(passphrase: str, ciphertext: str) -> str:
 	# Decode hexadecimals
 	ciphertext = unhexlify(ciphertext)
@@ -55,16 +60,18 @@ def decrypt(passphrase: str, ciphertext: str) -> str:
 	aes = AESGCM(key)
 	plaintext = aes.decrypt(iv, ciphertext, None)
 
+	# Return decrypted output in string of UTF-8 characters
 	return plaintext.decode("utf8")
 
 if __name__ == '__main__':
 
-	if os.path.isfile(args.inf):
+	# Get input string
+	if args.text:
+		indata = input("Enter plaintext to encrypt/decrypt: ")
+		printout = True
+	elif os.path.isfile(args.inf):
 		infile = open(args.inf, 'r+')
 		indata = infile.read()
-	elif args.text:
-		indata = input("Enter plaintext to encrypt: ")
-		printout = True
 	else:
 		print(f'File not found: {args.inf}')
 		quit(1)
@@ -75,13 +82,16 @@ if __name__ == '__main__':
 	else:
 		output = encrypt(getpass(), indata)
 
-	# Quit before writing out if -t
+	# Write output string to stdout
 	if args.text:
-		print(output)
+		sys.stdout.write(output + '\n')
 		quit(0)
+	# Open file for writing
 	elif os.path.isfile(args.outf):
 		outfile = open(args.outf, 'w')
+	# Create file for writing
 	else:
 		outfile = open(args.outf, 'x')
 
+	# Write output to file opened/created
 	outfile.write(output)
